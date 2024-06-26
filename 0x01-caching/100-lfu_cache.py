69603b5166e692148ceee4831c9b37075ead829d
Dayvid0063 <orjidavid63@gmail.com>
2024-06-26 20:46:08 +0100

    AD

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -21,27 +21,29 @@ class LFUCache(BaseCaching):
         Cache a key-value pair.
         Evicts LFU items if cache exceeds maximum capacity.
         """
-        size = len(self.cache_data)
         if key is None or item is None:
             return
 
-        if size >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-            lfu = min(self.frequency.values())
-            lfk = [u for u, v in self.frequency.items() if v == lfu]
+        if (len(self.cache_data) >= BaseCaching.MAX_ITEMS and
+                key not in self.cache_data):
+            freq_u = min(self.frequency.values())
+            freq_k = [u for u, v in self.frequency.items() if v == freq_u]
 
-            if len(lfk) > 1:
-                l_rf = {u: self.usage[u] for u in lfk}
-                rmv = min(l_rf, key=l_rf.get)
+            if len(freq_k) > 1:
+                l_rf = {
+                    u: idx for idx, u in enumerate(self.usage) if u in freq_k
+                }
+                rmv_k = min(l_rf, key=l_rf.get)
             else:
-                rmv = lfk[0]
+                rmv_k = freq_k[0]
 
-            print(f"DISCARD: {rmv}")
-            del self.cache_data[rmv]
-            del self.frequency[rmv]
-            del self.usage[rmv]
+            print(f"DISCARD: {rmv_k}")
+            del self.cache_data[rmv_k]
+            del self.frequency[rmv_k]
+            self.usage.pop(rmv_k)
 
         if key in self.cache_data:
-            del self.usage[key]
+            self.usage.pop(key)
         self.usage[key] = None
         self.frequency[key] += 1
         self.cache_data[key] = item
@@ -51,9 +53,10 @@ class LFUCache(BaseCaching):
         Gets value linked to given key, or None if key not found.
         Update usage and frequency for accessed key.
         """
-        if key in self.cache_data:
-            del self.usage[key]
-            self.usage[key] = None
-            self.frequency[key] += 1
-            return self.cache_data[key]
-        return None
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.usage.pop(key)
+        self.usage[key] = None
+        self.frequency[key] += 1
+        return self.cache_data[key]
