4ea6040bab2cc3860aee5b76b60e6b29900d9555
Dayvid0063 <orjidavid63@gmail.com>
2024-07-04 01:36:56 +0100

    i18n tasks

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
+
+
 """Flask app"""
 
 
 from flask import Flask
 from flask import render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """Babel Configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """Route handler"""
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
